7fb585374372acf67aa1a11f1b3f83517f2f242d
pournimaanjole <pournimaanjole@gmail.com>
2024-05-22 08:56:03 +0530

    img is commented

--- src/components/WorkCard/WorkCard.tsx
+++ src/components/WorkCard/WorkCard.tsx
@@ -10,7 +10,7 @@ const WorkCard: React.FC<WorkBalanceProps>  = ({ imgages, title, text, altText }
   return (
     <div className='card'>
       
-    <img src={imgages} alt={altText}  className='cardImag'/>
+    {/* <img src={imgages} alt={altText}  className='cardImag'/> */}
     <h3>{title}</h3>
     <h4>{text}</h4>
     </div>
