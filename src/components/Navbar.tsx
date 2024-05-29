ed87b8c69ebd904792fcc0addae18da10301d688
Mahesh Shinde <97420827+maheshshinde140@users.noreply.github.com>
2024-05-29 20:29:10 +0530

    all clear and added pages

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -20,19 +20,12 @@ function Navbar({ className }: { className?: string }) {
             </Link>
           </div>
 
-          <div className="hidden lg:flex items-center">
-            <Menu
-              setActive={setActive}
-              className={isMenuOpen ? "block" : "hidden lg:flex "}
-            >
-              <MenuItem
-                setActive={setActive}
-                active={active}
-                item="What we do >"
-              >
+          <div className="hidden lg:flex items-center justify-center flex-grow">
+            <Menu setActive={setActive} className={isMenuOpen ? "block" : "hidden lg:flex"}>
+              <MenuItem setActive={setActive} active={active} item="What we do >">
                 <div className="flex justify-around space-y-6 space-x-10 p-10 rounded">
                   <div className="flex flex-col space-y-4 py-7">
-                    <p className="mb-2 text-xl text-slate-400 hover:text-yellow-400 hover:cursor-text ">
+                    <p className="mb-2 text-xl text-slate-400 hover:text-yellow-400 hover:cursor-text">
                       What we do<span>➡️</span>
                     </p>
                     <HoveredLink href="/service">
@@ -47,7 +40,7 @@ function Navbar({ className }: { className?: string }) {
                         Research & Innovations
                       </span>
                     </HoveredLink>
-                    <HoveredLink href="/courses">
+                    <HoveredLink href="/security">
                       <span className="hover:border-b-4 animate-in hover:text-blue-400 border-neutral-100 border-y-gray-400">
                         Security
                       </span>
@@ -55,9 +48,7 @@ function Navbar({ className }: { className?: string }) {
                   </div>
                   <div className="flex flex-col space-y-4 py-7">
                     <HoveredLink href="/courses">
-                      <span className="text-sm text-slate-400">
-                        Media & Investors
-                      </span>
+                      <span className="text-sm text-slate-400">Media & Investors</span>
                     </HoveredLink>
                     <HoveredLink href="/courses">
                       <span className="hover:border-b-4 animate-in hover:text-blue-400 border-neutral-100 border-y-gray-400">
@@ -65,71 +56,47 @@ function Navbar({ className }: { className?: string }) {
                       </span>
                     </HoveredLink>
                     <HoveredLink href="/courses">
-                      <span className="hover:border-b-4 hover:text-blue-400">
-                        Investor Relations
-                      </span>
+                      <span className="hover:border-b-4 hover:text-blue-400">Investor Relations</span>
                     </HoveredLink>
                   </div>
                 </div>
               </MenuItem>
-              <Link href={"/"}>
-                <MenuItem
-                  setActive={setActive}
-                  active={active}
-                  item="What we think"
-                />
+              <Link href={"/whatweThink"}>
+                <MenuItem setActive={setActive} active={active} item="What we think" />
               </Link>
-              <MenuItem
-                setActive={setActive}
-                active={active}
-                item="What we are >"
-              >
+              <MenuItem setActive={setActive} active={active} item="What we are >">
                 <div className="flex justify-around space-y-6 space-x-10 p-10 rounded">
                   <div className="flex flex-col space-y-4 ">
-                    <p className="mb-2 text-xl text-slate-400 hover:text-yellow-400 hover:cursor-pointer ">
+                    <p className="mb-2 text-xl text-slate-400 hover:text-yellow-400 hover:cursor-pointer">
                       About Arohi Software <span>➡️</span>
                     </p>
                     <HoveredLink href="/courses">
-                      <span className="text-sm text-slate-400">
-                        Our organization
-                      </span>
+                      <span className="text-sm text-slate-400">Our organization</span>
                     </HoveredLink>
                     <HoveredLink href="/courses">
-                      <span className="hover:border-b-4 hover:text-blue-400">
-                        Leaders
-                      </span>
+                      <span className="hover:border-b-4 hover:text-blue-400">Leaders</span>
                     </HoveredLink>
                     <HoveredLink href="/courses">
-                      <span className="hover:border-b-4 hover:text-blue-400">
-                        Locations
-                      </span>
+                      <span className="hover:border-b-4 hover:text-blue-400">Locations</span>
                     </HoveredLink>
                     <HoveredLink href="/gallery">
-                      <span className="hover:border-b-4 hover:text-blue-400">
-                        Gallery
-                      </span>
+                      <span className="hover:border-b-4 hover:text-blue-400">Gallery</span>
                     </HoveredLink>
                     <HoveredLink href="/events">
-                      <span className="hover:border-b-4 hover:text-blue-400">
-                        Events
-                      </span>
+                      <span className="hover:border-b-4 hover:text-blue-400">Events</span>
                     </HoveredLink>
                   </div>
                   <div className="flex flex-col space-y-4 pt-7">
                     <HoveredLink href="/courses">
-                      <span className="text-sm text-slate-400">
-                        Media & Investors
-                      </span>
+                      <span className="text-sm text-slate-400">Media & Investors</span>
                     </HoveredLink>
                     <HoveredLink href="/courses">
-                      <span className="hover:border-b-4 animate-in border-neutral-100 border-y-gray-400  hover:text-blue-400">
+                      <span className="hover:border-b-4 animate-in border-neutral-100 border-y-gray-400 hover:text-blue-400">
                         Media Relations
                       </span>
                     </HoveredLink>
                     <HoveredLink href="/courses">
-                      <span className="hover:border-b-4  hover:text-blue-400">
-                        Investor Relations
-                      </span>
+                      <span className="hover:border-b-4 hover:text-blue-400">Investor Relations</span>
                     </HoveredLink>
                   </div>
                 </div>
@@ -137,13 +104,11 @@ function Navbar({ className }: { className?: string }) {
               <MenuItem setActive={setActive} active={active} item="Careers >">
                 <div className="flex justify-around space-y-6 space-x-10 p-10 rounded">
                   <div className="flex flex-col space-y-4">
-                    <p className="mb-2 text-xl  text-slate-400 hover:text-yellow-400 hover:cursor-text ">
+                    <p className="mb-2 text-xl text-slate-400 hover:text-yellow-400 hover:cursor-text">
                       Careers home <span>➡️</span>
                     </p>
                     <HoveredLink href="/courses">
-                      <span className="text-sm text-slate-400 cursor-text">
-                        Find Jobs
-                      </span>
+                      <span className="text-sm text-slate-400 cursor-text">Find Jobs</span>
                     </HoveredLink>
                     <HoveredLink href="/courses">
                       <span className="hover:border-b-4 animate-in hover:text-blue-400 border-neutral-100 border-y-gray-400">
@@ -163,9 +128,7 @@ function Navbar({ className }: { className?: string }) {
                   </div>
                   <div className="flex flex-col space-y-4 pt-7">
                     <HoveredLink href="/courses">
-                      <span className="text-sm text-slate-400 cursor-text">
-                        Training & Internships
-                      </span>
+                      <span className="text-sm text-slate-400 cursor-text">Training & Internships</span>
                     </HoveredLink>
                     <HoveredLink href="/courses">
                       <span className="hover:border-b-4 animate-in hover:text-blue-400 border-neutral-100 border-y-gray-400">
@@ -173,14 +136,10 @@ function Navbar({ className }: { className?: string }) {
                       </span>
                     </HoveredLink>
                     <HoveredLink href="/contact">
-                      <span className="text-sm pt-2 text-slate-400 hover:text-yellow-500 cursor-pointer">
-                        Contact Us
-                      </span>
+                      <span className="text-sm pt-2 text-slate-400 hover:text-yellow-500 cursor-pointer">Contact Us</span>
                     </HoveredLink>
                     <HoveredLink href="/faq">
-                      <span className="text-sm text-slate-400 hover:text-yellow-500 cursor-pointer">
-                        FAQ
-                      </span>
+                      <span className="text-sm text-slate-400 hover:text-yellow-500 cursor-pointer">FAQ</span>
                     </HoveredLink>
                   </div>
                 </div>
@@ -188,49 +147,69 @@ function Navbar({ className }: { className?: string }) {
             </Menu>
           </div>
 
-          {/* Burger icon for small screens */}
-          <div className="block lg:hidden">
-            <button
-              onClick={() => setIsMenuOpen(!isMenuOpen)}
-              className="text-white focus:outline-none"
-              aria-label="Toggle Menu"
-            >
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                className="h-6 w-6"
-                fill="none"
-                viewBox="0 0 24 24"
-                stroke="currentColor"
+          {/* Search Icon and Burger icon for small screens */}
+          <div className="flex items-center space-x-4">
+            <div className="hidden lg:block">
+              <button
+                className="text-white focus:outline-none"
+                aria-label="Search"
               >
-                {isMenuOpen ? (
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  className="h-6 w-6"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+                >
                   <path
                     strokeLinecap="round"
                     strokeLinejoin="round"
                     strokeWidth={2}
-                    d="M6 18L18 6M6 6l12 12"
+                    d="M10 4a6 6 0 100 12 6 6 0 000-12zM21 21l-4.35-4.35"
                   />
-                ) : (
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    strokeWidth={2}
-                    d="M4 6h16M4 12h16m-7 6h7"
-                  />
-                )}
-              </svg>
-            </button>
+                </svg>
+              </button>
+            </div>
+
+            <div className="block lg:hidden">
+              <button
+                onClick={() => setIsMenuOpen(!isMenuOpen)}
+                className="text-white focus:outline-none"
+                aria-label="Toggle Menu"
+              >
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  className="h-6 w-6"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+                >
+                  {isMenuOpen ? (
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      strokeWidth={2}
+                      d="M6 18L18 6M6 6l12 12"
+                    />
+                  ) : (
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      strokeWidth={2}
+                      d="M4 6h16M4 12h16m-7 6h7"
+                    />
+                  )}
+                </svg>
+              </button>
+            </div>
           </div>
         </div>
 
-        {/* Mobile menu */}
+        {/* Mobile Menu */}
         {isMenuOpen && (
           <div className="lg:hidden">
-            <Menu setActive={setActive} className="block">
-              <MenuItem
-                setActive={setActive}
-                active={active}
-                item="What we do >"
-              >
+            <Menu setActive={setActive} className={isMenuOpen ? "block" : "hidden lg:flex"}>
+              <MenuItem setActive={setActive} active={active} item="What we do >">
                 <div className="flex flex-col space-y-4 py-7">
                   <HoveredLink href="/service">
                     <span className="text-sm text-slate-400">Services</span>
@@ -240,7 +219,7 @@ function Navbar({ className }: { className?: string }) {
                       Research & Innovations
                     </span>
                   </HoveredLink>
-                  <HoveredLink href="/courses">
+                  <HoveredLink href="/security">
                     <span className="text-sm text-slate-400">Security</span>
                   </HoveredLink>
                   <HoveredLink href="/courses">
@@ -260,7 +239,7 @@ function Navbar({ className }: { className?: string }) {
                   </HoveredLink>
                 </div>
               </MenuItem>
-              <Link href={"/"}>
+              <Link href={"/whatweThink"}>
                 <MenuItem
                   setActive={setActive}
                   active={active}
