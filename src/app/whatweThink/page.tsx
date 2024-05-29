33872d81a7a6a106d4ebfbc6a1d5b19fb00c11ce
pournimaanjole <pournimaanjole@gmail.com>
2024-05-29 11:43:23 +0530

    security page is completed

---
+++ src/app/whatweThink/page.tsx
@@ -0,0 +1,39 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <div>
+     
+     <div className='  bg-black h-24'>
+
+</div>
+
+<div className=" sm:h-[85vh]  h-[50vh] w-full pt-7 " style={{
+                  backgroundImage:
+                    "url('https://img.freepik.com/free-photo/flat-lay-workstation-with-copy-space-laptop_23-2148430879.jpg')",
+                    backgroundSize: "cover",
+                  backgroundPosition: "center",
+                  backgroundRepeat: "no-repeat",
+                }}>
+                  <div className='container  '>
+                    <div className='container  mx-10   font-bold text-white-100 border-b-2 border-b-slate-300 py-3'>
+                      <p > Stay ahead of change</p>
+                    </div>
+                    <div className='container mt-6'>
+                      <p className=' font-normal text-lg sm:text-3xl '>
+                      Explore our research, insights, and examples of real  <br/>  client impact, designed to help you embrace the key forces  <br/>  of change and get to value faster.
+                      </p>
+                    </div>
+
+                  </div>
+
+
+  
+</div>
+
+
+    </div>
+  )
+}
+
+export default page
